bac65bf01b469127f6d942af88e9d04dd50bfcc9
amr darawsheh <amr.1999@live.com>
2020-09-14 13:42:23 +0300

    Added and created a sample for amazon aws polly

---
+++ JobsAbility/JobsAbility/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobsAbility.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EF()
+        {
+            return View();
+        }
+        public IActionResult Polly()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Polly(string text)
+        {
+            var filename = Guid.NewGuid().ToString();
+            PollyHelper.TextToMp3Async(text,filename);
+            ViewBag.audio = filename+".mp3";
+            return View();
+        }
+    }
+}
---
+++ JobsAbility/JobsAbility/Models/jobsDBContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace JobsAbility.Models
+{
+    public partial class jobsDBContext : DbContext
+    {
+        public jobsDBContext()
+        {
+        }
+
+        public jobsDBContext(DbContextOptions<jobsDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=jobsabilitydb.cte2cofecdcz.us-east-2.rds.amazonaws.com,1433;Database=jobsDB;User Id=admin;Password=Enter123;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(10)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
