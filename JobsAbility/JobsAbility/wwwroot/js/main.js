08b93abe16d0cb09943dfc526629a14524936bea
Alina A <loltiI@hotmail.com>
2020-09-14 13:08:01 +0300

    - First commit

--- JobsAbility/JobsAbility/wwwroot/js/main.js
+++ JobsAbility/JobsAbility/wwwroot/js/main.js
@@ -1,11 +1,11 @@
-(function(a) {
+(function (a) {
     var b = a(window);
-    b.on("load", function() { a("#preloader").fadeOut("slow") });
-    b.on("scroll", function() { if (a(this).scrollTop() > 250) { a(".back-to-top").fadeIn(200) } else { a(".back-to-top").fadeOut(200) } });
-    a(".back-to-top").on("click", function() { a("html, body").animate({ scrollTop: 0 }, "slow"); return false });
+    b.on("load", function () { a("#preloader").fadeOut("slow") });
+    b.on("scroll", function () { if (a(this).scrollTop() > 250) { a(".back-to-top").fadeIn(200) } else { a(".back-to-top").fadeOut(200) } });
+    a(".back-to-top").on("click", function () { a("html, body").animate({ scrollTop: 0 }, "slow"); return false });
     var c = a(".mobile-menu").data("logo");
     a("#main-navbar").slicknav({ appendTo: ".mobile-menu", removeClasses: false, label: "", closedSymbol: '<i class="lni-chevron-right"><i/>', openedSymbol: '<i class="lni-chevron-down"><i/>', brand: '<a href="index.html"><img src="' + c + '" class="img-responsive" alt="logo"></a>' });
-    b.on("scroll", function() { var f = a(window).scrollTop(); if (f >= 100) { a(".scrolling-navbar").addClass("top-nav-collapse") } else { a(".scrolling-navbar").removeClass("top-nav-collapse") } });
+    b.on("scroll", function () { var f = a(window).scrollTop(); if (f >= 80) { a(".scrolling-navbar").addClass("top-nav-collapse"); } else { a(".scrolling-navbar").removeClass("top-nav-collapse"); } });
     if (a(".fact-count").length > 0) { a(".counter").counterUp({ delay: 10, time: 500 }) }
     var e = a("#testimonials");
     e.owlCarousel({ autoplay: true, margin: 30, dots: true, autoplayHoverPause: true, nav: false, loop: true, responsiveClass: true, responsive: { 0: { items: 1, }, 991: { items: 1 } } });
